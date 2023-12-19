3fd426de8c4bd5b19a710cffa5b0475b67482f13
xuanchen <98302650+yaslydia@users.noreply.github.com>
2023-12-18 21:17:08 -0800

    Added side bar

---
+++ src/components/ProjectSidebar.jsx
@@ -0,0 +1,10 @@
+export default function ProjectSidebar() {
+  return (
+    <aside>
+      <h2>Project Name</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+    </aside>
+  );
+}
