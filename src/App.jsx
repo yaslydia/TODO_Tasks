11f040134a2bcbba917295c06cc1665410a12a49
xuanchen <98302650+yaslydia@users.noreply.github.com>
2023-12-21 20:30:45 -0800

    Added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectSidebar from "./components/ProjectSidebar.jsx";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -1,3 +1,5 @@
+import Button from "./Button.jsx";
+
 export default function ProjectSidebar() {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
@@ -5,9 +7,9 @@ export default function ProjectSidebar() {
         Project Name
       </h2>
       <div>
-        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 hover:bg-stone-600 hover:text-stone-100">
+        <Button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 hover:bg-stone-600 hover:text-stone-100">
           + Add Project
-        </button>
+        </Button>
       </div>
     </aside>
   );
