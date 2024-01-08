5f292d59fbd541421fd5cd3eb88b233907380de2
xuanchen <98302650+yaslydia@users.noreply.github.com>
2024-01-08 13:00:52 -0800

    enabled cancel added project

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,8 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
 
+import Button from "./Button.jsx";
+
 const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
   const dialog = useRef();
   useImperativeHandle(ref, () => {
@@ -15,8 +17,8 @@ const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
       ref={dialog}
       className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
       {children}
-      <form method="dialog">
-        <button>{buttonCaption}</button>
+      <form method="dialog" className="mt-4 text-right">
+        <Button>{buttonCaption}</Button>
       </form>
     </dialog>,
     document.getElementById("modal-root")
